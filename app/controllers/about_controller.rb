da5f628dd7648e840d017b4c5ebe95bdd1e6bcb8
Ricardo Fearing <ricardo@heysparkbox.com>
2022-03-03 07:56:24 -0500

    create an about route

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+# Must be the name of the file, but in camelCase and extend ApplicationController
+class AboutController < ApplicationController
+	def index
+		# If this renders anything, use it, otherwise use default ApplicationController renderer
+	end
+end
