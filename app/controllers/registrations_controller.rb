1e4e32571b9736126b6fedf6f680f0a0f656a83e
Ricardo Fearing <ricardo@heysparkbox.com>
2022-03-07 23:15:45 -0500

    Initial create user and errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,19 @@ class RegistrationsController < ApplicationController
 		# @ = Instance variable (will be available in our view)
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			render_to root_path, notice: "Successfully Created User"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
 end
